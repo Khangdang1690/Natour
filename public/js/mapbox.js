59cf80075709fcf479b7bc5b0de805578da53edb
Khang Dang <khangdang0199@gmail.com>
2025-01-09 13:21:07 -0800

    Change token

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 export const displayMap = locations => {
-  mapboxgl.accessToken =
-    'pk.eyJ1Ijoiam9uYXNzY2htZWR0bWFubiIsImEiOiJjam54ZmM5N3gwNjAzM3dtZDNxYTVlMnd2In0.ytpI7V7w7cyT1Kq5rT9Z1A';
+  mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN;
 
   var map = new mapboxgl.Map({
     container: 'map',
